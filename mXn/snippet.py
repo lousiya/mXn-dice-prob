4cb002a10970c269f45673c3f994e9b5562d66fe
lousiya <geena1725@gmail.com>
2016-07-08 14:45:25 -0400

    Add files via upload

---
+++ mXn/snippet.py
@@ -0,0 +1,23 @@
+import random
+import sys
+sum=0
+m=input("enter the value of the row")
+n=input("enter the value of the column ")
+prod=m*n
+while(sum!=prod):
+    random_num=random.randrange(1,7)
+    print"the value of the rolled die is",random_num
+    if(sum+random_num<=prod):
+     sum=sum+random_num
+    x=sum%n
+    y=sum/n
+    
+    if(y%2==1):
+      x=n-1-x
+    print "sum=",sum
+    print"the position of the die is (x,y)=",x,y
+    print"\n"
+
+if(sum==prod):
+    print "game won"
+
